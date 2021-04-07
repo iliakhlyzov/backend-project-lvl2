5029630624f8ae56ab3f232187125a191b3074ae
iliakhlyzov <khlyzoff@gmail.com>
2021-04-07 15:24:57 +0300

    updated

--- __tests__/test.index.js
+++
@@ -1,7 +0,0 @@
-import genDiff from '../index.js';
-import { readFile } from '../index'
-
-test('diff correct', () => {
-  const diff = readFile('../__testfiles__/difference.json').toString();
-  expect(genDiff('../__testfiles__/file1.json', '../__testfiles__/file2.json')).toEqual(diff);
-})
